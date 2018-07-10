0c8755e9f884212507b26f9495cc4fc5d969e6a1
Steffen Buehl <steffen.buehl@aberle-software.com>
2018-07-10 11:05:06 +0200

    [TASK] update travis.sh for smb releases

--- travis.sh
+++ travis.sh
@@ -2,7 +2,7 @@
 abapmerge src/zabapgit.prog.abap > ../zabapgit.abap
 wc -l ../zabapgit.abap
 cd ..
-git clone https://github.com/abapGit/build.git
+git clone https://github.com/sb-sap/abapgit-build.git
 ls -l
 cp zabapgit.abap build/zabapgit.abap
 cd build
@@ -11,4 +11,4 @@ git config --global user.email "builds@travis-ci.com"
 git config --global user.name "Travis CI"
 git add zabapgit.abap
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER"
-git push -q https://$GITHUB_API_KEY@github.com/abapGit/build.git > /dev/null 2>&1
+git push -q https://$GITHUB_API_KEY@github.com/sb-sap/abapgit-build.git > /dev/null 2>&1
